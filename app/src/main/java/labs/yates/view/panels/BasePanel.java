YatesRocks/labs

commit c96fd0c587fe4476b5b125bd17ef6b3d66cb3d4b

Reworked BasePanel API, marked more stuff as deprecated

app/src/main/java/labs/yates/view/panels/BasePanel.java
@@ -10,7 +10,8 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public abstract class BasePanel extends JPanel {
-    private final JButton actionButton;
+    @Deprecated
+    private JButton actionButton;
 
     @Deprecated
     protected BasePanel(String actionCommand) {
@@ -18,18 +19,24 @@ public abstract class BasePanel extends JPanel {
         actionButton = new JButton(actionCommand);
     }
 
-    protected BasePanel(JButton actionButton) {
-        this.actionButton = actionButton;
-    }
+    protected BasePanel() {}
 
     /**
      * Creates a pane filled with buttons needed across every lab.
      * Stuff like close, menu, and an action button. The action
      * button must be configured through other methods.
+     * <hr>
+     * Deprecated in favor of the new API.
+     * This is marked for removal.
      *
      * @return A JPanel with generic buttons
      */
+    @Deprecated
     protected JPanel buttonPane() {
+        return buttonPane(actionButton);
+    }
+
+    protected JPanel buttonPane(JButton actionButton) {
         JPanel contentPane = new JPanel(new MigLayout());
         contentPane.setBorder(new LineBorder(Color.LIGHT_GRAY));
 
@@ -82,7 +89,7 @@ public abstract class BasePanel extends JPanel {
      * @param actionListener The action listener to apply
      * @return The customized button
      */
-    protected JButton button(String label, ActionListener actionListener) {
+    protected static JButton button(String label, ActionListener actionListener) {
         JButton open = new JButton(label);
         open.addActionListener(actionListener);
         return open;

app/src/main/java/labs/yates/view/panels/TagExtractor.java
@@ -23,18 +23,12 @@ public class TagExtractor extends BasePanel {
     private final FileDisplayer fileDisplayer = new FileDisplayer();
 
     public TagExtractor() {
-        super("Load stop words");
-        // TODO: Do stuff on load stop words click
-        setAction(e -> {
-            stopWordsPath = getFile(JFileChooser.FILES_ONLY).toPath();
-            textStripper = new TextStripper(stopWordsPath);
-        });
-
         // The file dock is for opening a new text file
         add(fileDock(), "dock north, push, grow");
         // Displays the file
         add(fileDisplayer, "gap 5 5 5 5, push, grow");
-        add(buttonPane(), "dock south, push, grow");
+        JButton loadStopWords = button("Load stop words", this::loadStopWords);
+        add(buttonPane(loadStopWords), "dock south, push, grow");
     }
 
     private JTextField pathField() {
@@ -56,8 +50,12 @@ public class TagExtractor extends BasePanel {
         return panel;
     }
 
-    private void save(ActionEvent e) {
+    private void loadStopWords(ActionEvent e) {
+        // TODO
+    }
 
+    private void save(ActionEvent e) {
+        // TODO
     }
 
     private void openFile(ActionEvent e) {

commit 2c004c780192014399b63b7f955e6f56f18564b7

Fixed bug I introduced in c96fd0c5

app/src/main/java/labs/yates/view/panels/BasePanel.java
@@ -19,7 +19,9 @@ public abstract class BasePanel extends JPanel {
         actionButton = new JButton(actionCommand);
     }
 
-    protected BasePanel() {}
+    protected BasePanel() {
+        setLayout(new MigLayout());
+    }
 
     /**
      * Creates a pane filled with buttons needed across every lab.